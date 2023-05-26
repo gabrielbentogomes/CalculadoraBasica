6cd2a72274726c8cf4ba9b0769418c835b0add5f
Gabriel <gabrielbgwork@gmail.com>
2023-05-25 21:57:54 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraBasica/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CalculadoraBasica
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(12, 185);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 31);
+            button1.TabIndex = 0;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(93, 185);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 31);
+            button2.TabIndex = 1;
+            button2.Text = "-";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(174, 185);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 31);
+            button3.TabIndex = 2;
+            button3.Text = "x";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(255, 185);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 31);
+            button4.TabIndex = 3;
+            button4.Text = "/";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(156, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 29);
+            label1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(121, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(121, 68);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(156, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 25);
+            label2.TabIndex = 7;
+            label2.Text = "=";
+            // 
+            // label3
+            // 
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(139, 119);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 29);
+            label3.TabIndex = 8;
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(345, 232);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ CalculadoraBasica/Form1.cs
@@ -0,0 +1,65 @@
+namespace CalculadoraBasica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "-";
+            float n1 = float.Parse(textBox1.Text);
+            float n2 = float.Parse(textBox2.Text);
+            float subtracao = n1 - n2;
+            label3.Text = subtracao.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "+";
+            float n1 = float.Parse(textBox1.Text);
+            float n2 = float.Parse(textBox2.Text);
+            float soma = n1 + n2;
+            label3.Text = soma.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "x";
+            float n1 = float.Parse(textBox1.Text);
+            float n2 = float.Parse(textBox2.Text);
+            float multiplicar = n1 * n2;
+            label3.Text = multiplicar.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "/";
+            float n1 = float.Parse(textBox1.Text);
+            float n2 = float.Parse(textBox2.Text);
+            float divisao = n1 / n2;
+            label3.Text = divisao.ToString();
+        }
+    }
+}
